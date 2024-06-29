34a37db3c5bea3658ca4f4fcadc966ba1c5150eb
Bruno Macedo <devbrunomacedo@gmail.com>
2024-06-29 12:46:32 -0300

    feat: add Componente Card

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,15 @@
+import "./style.css"
+
+export const Card = (props) => {
+    return (
+        <div className="card" id={props.identificador}>
+            <div className="title">
+                <h2>{props.title}</h2>
+                {props.icone}
+            </div>
+            <div className="values">
+                <span>{props.values}</span>
+            </div>    
+        </div>  
+    )
+}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,11 +2,29 @@ import './style.css'
 import React from 'react'
 
 import { Header } from '../../components/Header'
+import { Card } from '../../components/Card'
 
 export const Home = () => {
     return (
         <>
-            <Header />     
+            <Header />   
+            
+            <main>
+                <div className='box-cards'>
+                    <Card 
+                        title={"Entradas"} 
+                        icone={<i class="bi bi-arrow-up-circle-fill"></i>} values={1920.98} 
+                        identificador='entradas'
+                    />
+
+                    <Card 
+                        title={"Saidas"} 
+                        icone={<i class="bi bi-arrow-down-circle-fill"></i>} values={1540.28} 
+                        identificador='saidas'
+                    />
+                </div>
+    
+            </main>     
         </>
     )
 }
